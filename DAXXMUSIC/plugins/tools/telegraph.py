844cc331aa012650524fc9ea1c26d5680bc3281c
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-12-20 19:39:13 +0530

    Update telegraph.py

--- DAXXMUSIC/plugins/tools/telegraph.py
+++ DAXXMUSIC/plugins/tools/telegraph.py
@@ -1,35 +1,86 @@
-from telegraph import upload_file
+import os
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from DAXXMUSIC import app
-from pyrogram.types import InputMediaPhoto
+import requests
 
-@app.on_message(filters.command(["tgm", "telegraph"]))
-async def ul(_, message):
-    reply = message.reply_to_message
-    if reply and reply.media:
-        i = await message.reply("⏳")
-        path = await reply.download()  # Download media file
+
+def upload_file(file_path):
+    url = "https://catbox.moe/user/api.php"
+    data = {"reqtype": "fileupload", "json": "true"}
+    files = {"fileToUpload": open(file_path, "rb")}
+    response = requests.post(url, data=data, files=files)
+
+    if response.status_code == 200:
+        return True, response.text.strip()
+    else:
+        return False, f"ᴇʀʀᴏʀ: {response.status_code} - {response.text}"
+
+
+@app.on_message(filters.command(["tgm", "tgt", "telegraph", "tl"]))
+async def get_link_group(client, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "❍ ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇᴅɪᴀ ᴛᴏ ᴜᴘʟᴏᴀᴅ ᴏɴ ᴛᴇʟᴇɢʀᴀᴘʜ"
+        )
+
+    media = message.reply_to_message
+    file_size = 0
+    if media.photo:
+        file_size = media.photo.file_size
+    elif media.video:
+        file_size = media.video.file_size
+    elif media.document:
+        file_size = media.document.file_size
+
+    if file_size > 200 * 1024 * 1024:
+        return await message.reply_text("Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴍᴇᴅɪᴀ ғɪʟᴇ ᴜɴᴅᴇʀ 200MB.")
+
+    try:
+        text = await message.reply("❍ ᴘʀᴏᴄᴇssɪɴɢ...")
+
+        async def progress(current, total):
+            try:
+                await text.edit_text(f"❍ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ... {current * 100 / total:.1f}%")
+            except Exception:
+                pass
+
         try:
-            # Upload the file to Telegraph
-            result = upload_file(path)
-            url = "https://telegra.ph" + result[0]
-            await i.edit(f'ᴜʀ ʟɪɴᴋ: {url}')
+            local_path = await media.download(progress=progress)
+            await text.edit_text("❍ ᴜᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
+
+            success, upload_path = upload_file(local_path)
+
+            if success:
+                await text.edit_text(
+                    f"❍ | [ᴛᴀᴘ ᴛʜᴇ ʟɪɴᴋ]({upload_path})",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton(
+                                    "ADD ME",
+                                    url=https://t.me/kira_probot?startgroup=true,
+                                )
+                            ]
+                        ]
+                    ),
+                )
+            else:
+                await text.edit_text(
+                    f"❍ ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ᴜᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ғɪʟᴇ\n{upload_path}"
+                )
+
+            try:
+                os.remove(local_path)
+            except Exception:
+                pass
+
         except Exception as e:
-            await i.edit(f"Error: {e}")
-
-###
-
-@app.on_message(filters.command(["graph", "grf"]))
-async def ul(_, message):
-    reply = message.reply_to_message
-    if reply and reply.media:
-        i = await message.reply("⏳")
-        path = await reply.download()  # Download media file
-        try:
-            # Upload the file to Graph.org
-            result = upload_file(path)
-            url = "https://graph.org" + result[0]
-            await i.edit(f'ᴜʀ ʟɪɴᴋ: {url}')
-        except Exception as e:
-            await i.edit(f"Error: {e}")
-
+            await text.edit_text(f"❍ ғɪʟᴇ ᴜᴘʟᴏᴀᴅ ғᴀɪʟᴇᴅ\n\n❍ <i>ʀᴇᴀsᴏɴ: {e}</i>")
+            try:
+                os.remove(local_path)
+            except Exception:
+                pass
+            return
+    except Exception:
+        pass
