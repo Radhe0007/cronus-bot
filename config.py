792d0866b03b9f6d48401144a6676040907c4210
Radhe0007 <rajprince5670@gmail.com>
2024-12-31 09:43:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/un-bots/cronus",
+    "https://github.com/Radhe0007/cronus-bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
